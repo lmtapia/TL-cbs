0593bb8925f0ee1459026b21f0191e2a9b5e6b7f
TapiaX <luistapia2998@gmail.com>
2022-08-23 04:43:03 -0400

    var calling works, updated TL-Editor

--- TL-Tests/regen-funcons.sh
+++ TL-Tests/regen-funcons.sh
@@ -1,8 +1,10 @@
-# Example usage:
-#   ./regen-funcons.sh Coverage
+#!/bin/bash
+
+fileext="tl"
+lang="TL"
 
 SpooDir="../../../cbs-beta-tools"
 
-echo "Generating funcons for all .tl files in ./$1"
+echo "Generating funcons for all .$fileext files in ./$1"
 #java -jar ${SpooDir}/spoofax-sunshine.jar --auto-lang ${SpooDir}/include --project "$1" --builder "Generate Funcons" --build-on-all ./ --non-incremental
-java -jar ${SpooDir}/sunshine2.jar build -l ../TL-Editor/target -p "$1" -n "Generate Funcons"
+java -jar ${SpooDir}/sunshine2.jar build -l ../${lang}-Editor/target -p "$1" -n "Generate Funcons"
