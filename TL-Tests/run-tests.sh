fea9ea7a8efd8d1b67044daf925ca273559dd61b
TapiaX <luistapia2998@gmail.com>
2022-08-23 04:43:03 -0400

    var calling works, updated TL-Editor

--- TL-Tests/run-tests.sh
+++ TL-Tests/run-tests.sh
@@ -8,11 +8,11 @@ function run-test {
   testTitle=$1
   testNumber=$2
   testDir=$3
-  echo "$testNumber ${testTitle}:"
-  echo "===== Program =====" > ${testDir}${testTitle}.output
-  cat ${testDir}${testTitle}.$fileext >> ${testDir}${testTitle}.output
-  echo $'\n'"===================" >> ${testDir}${testTitle}.output
-  ${runfct} --config ${lang}.config ${testDir}${testTitle}.fct >> ${testDir}/${testTitle}.output
+  echo "**$testNumber ${testTitle}:"
+  echo "===== Program =====" | tee ${testDir}${testTitle}.output
+  cat ${testDir}${testTitle}.$fileext | tee -a ${testDir}${testTitle}.output
+  echo $'\n'"===================" | tee -a ${testDir}${testTitle}.output
+  ${runfct} --config ${lang}.config ${testDir}${testTitle}.fct | tee -a ${testDir}/${testTitle}.output
 }
 i=1
 for dir in $lang-*/;
