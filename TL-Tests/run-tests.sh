79dd5a4f678cad9835dd5a71fdc9b3402fa32cac
TapiaX <luistapia2998@gmail.com>
2022-08-28 04:49:46 -0400

    setting expected results in *.lock files

--- TL-Tests/run-tests.sh
+++ TL-Tests/run-tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# usage: run-tests
+# option -c only compares without running funcon terms
 
 runfct="runfct"
 fileext="tl"
@@ -8,21 +10,29 @@ function run-test {
   testTitle=$1
   testNumber=$2
   testDir=$3
-  echo "**$testNumber ${testTitle}:"
-  echo "===== Program =====" | tee ${testDir}${testTitle}.output
-  cat ${testDir}${testTitle}.$fileext | tee -a ${testDir}${testTitle}.output
+  echo **$testNumber
+  echo "${testTitle}:" | tee ${testDir}${testTitle}.output
+  echo "===== Program ====="  # | tee ${testDir}${testTitle}.output
+  cat ${testDir}${testTitle}.$fileext # | tee -a ${testDir}${testTitle}.output
   echo $'\n'"===================" | tee -a ${testDir}${testTitle}.output
   ${runfct} --config ${lang}.config ${testDir}${testTitle}.fct | tee -a ${testDir}/${testTitle}.output
 }
 i=1
+errors=0
 for dir in $lang-*/;
 do
   echo -e "$dir"
-  for file in $dir*.fct;
+  for filefct in $dir*.fct;
   do
-    run-test $(basename $file  .fct) "Test0${i}" "$dir" 
+    file=$(basename $filefct  .fct)
+    if [[ -z "$1" || "$1" != "-c" ]]; then
+      run-test $file "Test ${i}" "$dir" 
+    fi
+    colordiff -q $dir${file}.output $dir${file}.output.lock
+    if [[ $? != 0 ]]; then
+      ((errors=errors+1))
+    fi
     ((i=i+1))
   done
 done
-
-#for v in *.txt ; do mv $v  $(basename $v  .txt); done
+echo "***********" Failed tests: $errors/$((i - 1))
