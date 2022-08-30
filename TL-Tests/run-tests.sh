a14756379eadbad9a14e798881edd267a599e2d9
TapiaX <luistapia2998@gmail.com>
2022-08-30 17:52:20 -0400

    Test scripts updated and customized, 19/19 Tests passed

--- TL-Tests/run-tests.sh
+++ TL-Tests/run-tests.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
-# usage: run-tests
+# usage: run-tests [file.fct] [-c] 
+# without filename runs all compatible test cases
 # option -c only compares without running funcon terms
 
 runfct="runfct"
 fileext="tl"
 lang="TL"
+dTool="colordiff"
+
+$dTool --help &> /dev/null
+if [[ $? != 0 ]]; then
+  dTool="diff" 
+fi
+
+i=1
+errors=0
 
 function run-test {
   testTitle=$1
   testNumber=$2
   testDir=$3
-  echo **$testNumber
-  echo "${testTitle}:" | tee ${testDir}${testTitle}.output
-  echo "===== Program ====="  # | tee ${testDir}${testTitle}.output
-  cat ${testDir}${testTitle}.$fileext # | tee -a ${testDir}${testTitle}.output
-  echo $'\n'"===================" | tee -a ${testDir}${testTitle}.output
-  ${runfct} --config ${lang}.config ${testDir}${testTitle}.fct | tee -a ${testDir}/${testTitle}.output
+  echo **$testNumber ${testDir}${testTitle}
+  if [[ -z "$4" || "$4" != "-c" ]]; then
+    echo "${testTitle}:" | tee ${testDir}${testTitle}.output
+    echo "===== Program ====="  # | tee ${testDir}${testTitle}.output
+    cat ${testDir}${testTitle}.$fileext # | tee -a ${testDir}${testTitle}.output
+    echo $'\n'"===================" | tee -a ${testDir}${testTitle}.output
+    ${runfct} --config ${lang}.config ${testDir}${testTitle}.fct | tee -a ${testDir}${testTitle}.output
+  fi
+  $dTool ${testDir}${testTitle}.output ${testDir}${testTitle}.output.lock
+  if [[ $? != 0 ]]; then
+    ((errors=errors+1))
+  fi
+  ((i=i+1))
 }
-i=1
-errors=0
-for dir in $lang-*/;
-do
-  echo -e "$dir"
-  for filefct in $dir*.fct;
+
+
+
+if [[ -n "$1" && -f $1 ]]; then
+  run-test $(basename $1 .fct) "Test ${i}" $(dirname $1)/ $2
+else  
+  for dir in $lang-*/;
   do
-    file=$(basename $filefct  .fct)
-    if [[ -z "$1" || "$1" != "-c" ]]; then
-      run-test $file "Test ${i}" "$dir" 
-    fi
-    colordiff -q $dir${file}.output $dir${file}.output.lock
-    if [[ $? != 0 ]]; then
-      ((errors=errors+1))
-    fi
-    ((i=i+1))
+    echo -e "$dir"
+    for filefct in $dir*.fct;
+    do
+      file=$(basename $filefct .fct)
+      run-test $file "Test ${i}" "$dir" $1
+    done
   done
-done
+fi
 echo "***********" Failed tests: $errors/$((i - 1))
